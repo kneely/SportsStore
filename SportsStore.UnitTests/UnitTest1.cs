c228edbbdc1047fbcf2a4ffe48d809681e02e631
Kevin <kevin.r.neely@outlook.com>
2018-05-29 11:39:07 -0400

    Lab1A complete

---
+++ SportsStore.UnitTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SportsStore.UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
